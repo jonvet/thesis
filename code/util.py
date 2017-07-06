2ab57724fcae6b3bd1cf158e9b7dcb69fe8c1e1c
jonvet <jonas.vetterle.16@ucl.ac.uk>
2017-07-06 12:05:51 +0100

    reordering folder

---
+++ code/util.py
@@ -0,0 +1,209 @@
+import nltk
+import tensorflow as tf
+import numpy as np
+import operator
+import pickle as pkl
+from collections import defaultdict
+import os
+import glob
+
+def word_vocab(path, vocab_name=None, vocab=None):
+
+    if vocab == None:
+        vocab = defaultdict(int)
+    count = 1
+    with open(path, 'r') as f:
+        for sentence in f:
+            print('\rSentence %d' %count, end='')
+            count += 1
+            this_sentence = sentence.split()
+            for word in this_sentence:
+                vocab[word] += 1
+    return vocab
+
+def finalise_vocab(raw_vocab, vocab_size):
+
+    '''
+    Takes as input a dictionary of unique words that occur in a corpus.
+    Returns a sorted dictionary of size vocab_size with unique values.
+    '''
+
+    sorted_vocab = sorted(raw_vocab.items(), key=operator.itemgetter(1))
+    sorted_vocab = sorted_vocab[-vocab_size:]
+    final_vocab = {'<PAD>': 0, '<OOV>': 1, '<END>': 2}
+    for word in sorted_vocab:
+        final_vocab[word[0]] = len(final_vocab)
+    return final_vocab
+
+def txt_to_row(path = '../corpus/gingerbread_corpus/rows/'):
+
+    '''
+    Takes as input a text file and outputs a text file with each sentence on a separate row
+    '''
+
+    temp_parts = glob.glob(path + '*.txt')
+    for part in temp_parts:
+        with open(part, 'r') as f:
+            data = f.read().replace('\n', ' ')
+        sentences = nltk.sent_tokenize(data)
+        with open(part[:-4] + '_rows.txt', 'w') as f:
+            for sentence in sentences:
+                f.write('%s\n' % sentence.lower())
+
+
+def txt_to_sent(path):
+
+    '''
+    Imports and tokenises a corpus.
+    Returns a list of sentences, where each sentence is itself a list of words
+    '''
+
+    sentences = []
+    tokenised_sentences = []
+    count = 1
+    with open(path, 'r') as f:
+        for sentence in f:
+            print('\rSentence %d' %count, end='')
+            this_sentence = sentence.split()
+            # this_sentence = nltk.word_tokenize(sentence)
+            tokenised_sentences.append(this_sentence)
+            count +=1
+    return tokenised_sentences
+
+    # with open(path, 'r') as f:
+    #     data = f.read().replace('\n', ' ')
+    # sentences = nltk.sent_tokenize(data)
+    # for sentence in sentences:
+    #     this_sentence = nltk.word_tokenize(sentence)
+    #     tokenised_sentences.append(this_sentence)
+    # return tokenised_sentences
+
+def sent_to_int(path, dictionary, max_sent_len, decoder=False):
+
+    '''
+    Encodes sentences using the vocabulary provided
+    If decoder = True, it will also return every sentences with a <GO> token in front, and every sentence with a <END> token at the end.
+    '''
+
+    lines = 0
+    with open(path, 'r') as f:
+        for line in f:
+            lines += 1
+    print('\n%d lines to do\n' % lines)
+
+    if decoder:
+        enc_sentences = np.full([lines, max_sent_len], dictionary['<PAD>'], dtype=np.int32)
+        dec_go_sentences = np.full([lines, max_sent_len + 2], dictionary['<PAD>'], dtype=np.int32)
+        dec_end_sentences = np.full([lines, max_sent_len + 2], dictionary['<PAD>'], dtype=np.int32)
+        sentence_lengths = np.full([lines], 0, dtype=np.int32)
+        dec_masks = np.full([lines, max_sent_len + 2], 0, dtype=np.int32)
+        
+    else:
+        enc_sentences = np.full([lines, max_sent_len], dictionary['<PAD>'], dtype=np.int32)
+        sentence_lengths = []
+
+    i = 0
+    if decoder:
+        with open(path, 'r') as f:
+            for sent in f:
+                print('\rSentence %d' %i, end='')
+                sentence = sent.split()
+                words = []
+                for word in sentence:
+                    if word not in dictionary:
+                        token_id = dictionary['<OOV>']
+                    else:
+                        token_id = dictionary[word]
+                    words.append(token_id)
+                sentence_lengths[i] = min(len(sentence),max_sent_len)
+                enc_sentences[i, 0:min(len(sentence),max_sent_len)] = words[:min(len(sentence),max_sent_len)]
+                dec_go_sentences[i, 1:min(len(sentence),max_sent_len)+1] = words[:min(len(sentence),max_sent_len)]
+                dec_go_sentences[i, min(len(sentence),max_sent_len)+1] = dictionary['<END>']
+                dec_end_sentences[i, 0:min(len(sentence),max_sent_len)] = words[:min(len(sentence),max_sent_len)]
+                dec_end_sentences[i, min(len(sentence),max_sent_len)] = dictionary['<END>']
+                dec_masks[i, 0:min(len(sentence),max_sent_len)+1] = 1
+                i += 1
+        return sentence_lengths, max_sent_len + 2, enc_sentences, dec_go_sentences, dec_end_sentences, dec_masks
+    
+    else:
+        with open(path, 'r') as f:
+            for sent in f:
+                print('\rSentence %d' %i, end='')
+                sentence = sent.split()
+                words = []
+                for word in sentence:
+                    if word not in dictionary:
+                        token_id = dictionary['<OOV>']
+                    else:
+                        token_id = dictionary[word]
+                    words.append(token_id)
+                enc_sentences[i, 0:min(len(sentence),max_sent_len)] = words[:min(len(sentence),max_sent_len)]
+                i += 1
+        return max_sent_len, enc_sentences, dec_go_sentences, dec_end_sentences
+
+
+def sick_encode(sentences, dictionary, embeddings = None):
+
+    '''
+    Encodes sentences using the vocabulary provided
+    '''
+
+    lines = len(sentences)
+    max_sent_len = -1
+    tokenised_sentences = []
+    for sentence in sentences:
+        this_sentence = nltk.word_tokenize(sentence)
+        tokenised_sentences.append(this_sentence)
+        length = len(this_sentence)
+        max_sent_len = length if length > max_sent_len else max_sent_len
+    print('\n%d lines to do\n' % lines)
+
+    i = 0
+    if type(embeddings) == np.ndarray:
+        dim = len(embeddings[0])
+        enc_sentences = np.full(
+            [lines, max_sent_len, dim], dictionary['<PAD>'], 
+            dtype=np.float32)
+        sentence_lengths = np.full(
+            [lines], 0, 
+            dtype=np.int32)
+        for sentence in tokenised_sentences:
+            print('\rSentence %d' %(i+1), end='')
+            words = []
+            for word in sentence:
+                if word not in dictionary:
+                    token_id = embeddings[dictionary['<OOV>'],:]
+                else:
+                    token_id = embeddings[dictionary[word],:]
+                words.append(token_id)
+            sentence_lengths[i] = min(len(sentence),max_sent_len)
+            enc_sentences[i, :min(len(sentence),max_sent_len), :] = np.array(words)[:min(len(sentence),max_sent_len), :]
+            i += 1
+    else:
+        enc_sentences = np.full(
+            [lines, max_sent_len], dictionary['<PAD>'], 
+            dtype=np.int32)
+        sentence_lengths = np.full(
+            [lines], 0, 
+            dtype=np.int32)
+        for sentence in tokenised_sentences:
+            print('\rSentence %d' %(i+1), end='')
+            words = []
+            for word in sentence:
+                if word not in dictionary:
+                    token_id = dictionary['<OOV>']
+                else:
+                    token_id = dictionary[word]
+                words.append(token_id)
+            sentence_lengths[i] = min(len(sentence),max_sent_len)
+            enc_sentences[i, 0:min(len(sentence),max_sent_len)] = words[:min(len(sentence),max_sent_len)]
+            i += 1
+    
+    return sentence_lengths, enc_sentences
+
+
+
+
+
+
+
---
+++ code/vocab_expansion.py
@@ -0,0 +1,129 @@
+import collections
+import os.path
+import gensim.models
+import numpy as np
+import sklearn.linear_model
+import tensorflow as tf
+import codecs
+from gensim.models import word2vec
+import pickle as pkl
+from skipthought import Skipthought_para
+from skipthought import Skipthought_model
+import operator
+
+# def get_glove_vocab(filename):
+#     vocab = collections.OrderedDict()
+#     numpy_arrays = []
+#     labels_array = []
+#     with codecs.open(vector_file, 'r', 'utf-8') as f:
+#         for c, r in enumerate(f):
+#             try:
+#                 sr = r.split()
+#                 labels_array.append(sr[0])
+#                 numpy_arrays.append( np.array([float(i) for i in sr[1:]]) )
+#             except:
+#                 print()
+#                 next
+#             if c == n_words:
+#                 return np.array( numpy_arrays ), labels_array
+#     return np.array( numpy_arrays ), labels_array
+
+def load_skip_thoughts_embeddings(path, epoch):
+
+    with open(path + 'vocab.pkl', 'rb') as f:
+        vocab = pkl.load(f)
+    tf.reset_default_graph()
+    model = Skipthought_model(
+        vocab = vocab, 
+        parameters = paras, 
+        path = path)
+    model.load_model(path, epoch)
+    embeddings = model.word_embeddings.eval(session = model.sess)
+
+    return embeddings
+
+def _expand_vocabulary(skip_thoughts_emb, skip_thoughts_vocab, word2vec):
+
+    # Find words shared between the two vocabularies.
+    print("Finding shared words")
+    shared_words = [w for w in word2vec.vocab if w in skip_thoughts_vocab]
+
+    # Select embedding vectors for shared words.
+    print("Selecting embeddings for %d shared words" % len(shared_words))
+    shared_st_emb = skip_thoughts_emb[[
+        skip_thoughts_vocab[w] for w in shared_words]]
+    shared_w2v_emb = word2vec[shared_words]
+
+    # Train a linear regression model on the shared embedding vectors.
+    print("Training linear regression model")
+    model = sklearn.linear_model.LinearRegression()
+    model.fit(shared_w2v_emb, shared_st_emb)
+
+    # Create the expanded vocabulary.
+    print("Creating embeddings for expanded vocabulary")
+    embedding_map = collections.OrderedDict()
+    print('Length of word2vec vocabulary: %d\n' % len(word2vec.vocab))
+    for i, w in enumerate(word2vec.vocab):
+        print('\rEmbedding %d' %(i+1), end='   ')
+    # Ignore words with underscores (spaces).
+        if "_" not in w:
+            w_emb = model.predict(word2vec[w].reshape(1, -1))
+            embedding_map[w] = w_emb.reshape(-1)
+
+    for w in skip_thoughts_vocab:
+        embedding_map[w] = skip_thoughts_emb[skip_thoughts_vocab[w]]
+
+    print("Created expanded vocabulary of %d words", len(embedding_map))
+
+    expanded_vocab = {}
+    expanded_embeddings = np.zeros([len(embedding_map), paras.embedding_size])
+
+    for i, w in enumerate(embedding_map.keys()):
+        expanded_vocab[w] = i
+        expanded_embeddings[i,:] = embedding_map[w]
+
+    print('Saving expanded vocab and embeddings')
+    with open(path + 'expanded_vocab.pkl', 'wb') as f:
+        pkl.dump(expanded_vocab, f)
+
+    embeddings_file = os.path.join(path, "expanded_embeddings.npy")
+    np.save(embeddings_file, expanded_embeddings)
+
+    return expanded_vocab, expanded_embeddings
+
+path = '../models/toronto_n4/'
+
+print('Loading trained skipthought word embeddings')
+with open(path + 'paras.pkl', 'rb') as f:
+    paras = pkl.load(f)
+skipthought_embeddings = load_skip_thoughts_embeddings(
+    path = path, 
+    epoch = 0)
+with open(path + 'vocab.pkl', 'rb') as f:
+    skipthought_vocab = pkl.load(f)
+
+if not (os.path.exists(path+'expanded_vocab.pkl') & os.path.exists(path+'expanded_embeddings.npy')):
+    
+    print('Loading skipthought vocabulary')
+    sorted_vocab = sorted(skipthought_vocab.items(),
+        key=operator.itemgetter(1))
+    skipthought_vocab = collections.OrderedDict(sorted_vocab)
+
+    print('Loading word2vec word embeddings')
+    word2vec = gensim.models.KeyedVectors.load_word2vec_format('/Users/Jonas/Documents/dev/GoogleNews-vectors-negative300.bin', binary=True) 
+
+    expanded_vocab, expanded_embeddings = _expand_vocabulary(
+        skipthought_embeddings, 
+        skipthought_vocab,
+        word2vec)
+else: 
+    print('Expanded vocab and embeddings already exist in %s' % path)
+
+# encas = model.encoded_sentences.eval(session = model.sess, feed_dict={model.graph.get_tensor_by_name('embedding_lookup:0'): test, model.sentences_lengths: test_lengths})
+
+
+
+
+
+
+
